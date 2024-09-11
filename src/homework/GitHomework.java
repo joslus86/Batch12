cc1265d8d8f71ecff233421ae1cc37c33cc0024d
natha <natha@DESKTOP-2SOTJIT>
2024-09-10 21:15:18 -0400

    noooooooooooo

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,35 +2,5 @@ package homework;
 
 public class GitHomework {
 	
-	public static void main(String[] args) {
-		System.out.println("Batch 12: Git Class Lecture");
-		int age= 12;
-		String name = "John";
-		//This is coming from github
-	}
 	
-	public static void add(int a, int b) {
-		a = 10;
-		b = 9;
-		System.out.println(a + b);
-	}
-	
-	public static void name(String string) {
-		string = "Jose";
-		System.out.println(string);
-	}
-	
-	public static void add(int a, int b, int c) {
-		a = 15;
-		b = 15;
-		c = 5;			
-		System.out.println(a + b + c);
-	}
-	
-	public static void name(String string, String string2) {
-		string = "Jose";
-		string2 = "Rojas";
-		System.out.println(string + string2);
-	}
-
 }
