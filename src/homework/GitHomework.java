25e263517b5abcddc856f430f4eae784c040e07c
natha <natha@DESKTOP-2SOTJIT>
2024-09-10 22:06:21 -0400

    hmmm

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,13 +2,4 @@ package homework;
 
 public class GitHomework {
 	
-	public void display() {
-		System.out.println("Jose Rojas");
-	}
-	/**	
-		This is coming form GitHub
-	Java is fun
-	Git is EASY
-	API is not fun...
-	*/
 }
