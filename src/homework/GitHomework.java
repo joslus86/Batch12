81d927478f93784e05d90e2960461dda419cc80d
natha <natha@DESKTOP-2SOTJIT>
2024-09-10 20:41:00 -0400

    added new method

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -5,5 +5,14 @@ public class GitHomework {
 	public static void main(String[] args) {
 		System.out.println("Batch 12: Git Class Lecture");
 	}
+	
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}
+	
+	public static void name(String string) {
+		string = "Jose";
+		System.out.println(string);
+	}
 
 }
